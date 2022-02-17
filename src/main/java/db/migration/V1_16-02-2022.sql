34304a9e2db2600e995da2c299654b6926f66bf4
guilherme <guilhermecerqueir4@gmail.com>
2022-02-17 02:04:42 -0300

    comment

--- src/main/java/db/migration/V1_16-02-2022.sql
+++ src/main/java/db/migration/V1_16-02-2022.sql
@@ -1,3 +1,5 @@
+
+/* A classe/tabela usuarios_acesso criou uma constraint UNIQUE que não deveria (mesmo estando FALSE no Spring). O script abaixo é para deletar esta constraint. */
 SELECT constraint_name from information_schema.constraint_column_usage 
 where table_name = 'usuarios_acesso' and column_name = 'acesso_id'
 and constraint_name <> 'unique_acesso_user';
