50dc8bd1cfb8b955fc9f97ccadb5a5bf6b2c1dc3
guilherme <guilhermecerqueir4@gmail.com>
2022-02-18 02:38:15 -0300

    novas classes

---
+++ src/main/java/cerqueir4/lojavirtual/model/CupomDesconto.java
@@ -0,0 +1,95 @@
+package cerqueir4.lojavirtual.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "cupom_desconto")
+@SequenceGenerator(name = "seq_cupom_desconto", sequenceName = "seq_cupom_desconto", allocationSize = 1, initialValue = 1)
+public class CupomDesconto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_cupom_desconto")
+	private Long id;
+	
+	private String codDesc;
+	private BigDecimal valorRealDesc;
+	private BigDecimal valorPorcentDesc;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dtValidade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCodDesc() {
+		return codDesc;
+	}
+
+	public void setCodDesc(String codDesc) {
+		this.codDesc = codDesc;
+	}
+
+	public BigDecimal getValorRealDesc() {
+		return valorRealDesc;
+	}
+
+	public void setValorRealDesc(BigDecimal valorRealDesc) {
+		this.valorRealDesc = valorRealDesc;
+	}
+
+	public BigDecimal getValorPorcentDesc() {
+		return valorPorcentDesc;
+	}
+
+	public void setValorPorcentDesc(BigDecimal valorPorcentDesc) {
+		this.valorPorcentDesc = valorPorcentDesc;
+	}
+
+	public Date getDtValidade() {
+		return dtValidade;
+	}
+
+	public void setDtValidade(Date dtValidade) {
+		this.dtValidade = dtValidade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CupomDesconto other = (CupomDesconto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+
+}
